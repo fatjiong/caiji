cbd46be10957cb562dd33c017a70f536f9a39ef5
fatjiong <757970599@qq.com>
2018-03-30 14:56:59 +0800

    [+] go版本第一次提交

---
+++ main.go
@@ -0,0 +1,137 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"strings"
+
+	"github.com/PuerkitoBio/goquery"
+	"github.com/axgle/mahonia"
+)
+
+type book struct {
+	author string
+	title string
+	urlList []string
+}
+
+type chapter struct {
+	title string
+	content string
+}
+
+
+//读取url中的信息
+func readUrlBody(url string) (string, error) {
+	resp, err := http.Get(url)
+	if err != nil {
+		return "err", err
+	}
+
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+
+	if err != nil {
+		return "err", err
+	}
+
+	return string(body), err
+}
+
+//字符串转换方法
+func ConvertToString(src string, srcCode string, tagCode string) string {
+	srcCoder := mahonia.NewDecoder(srcCode)
+	srcResult := srcCoder.ConvertString(src)
+	tagCoder := mahonia.NewDecoder(tagCode)
+	_, cdata, _ := tagCoder.Translate([]byte(srcResult), true)
+	result := string(cdata)
+	return result
+}
+
+//获取书籍信息(书名，作者，章节链接)
+func bookInfo(url string) *book{
+	doc, err := goquery.NewDocument(url)
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	//获取书名
+	title := doc.Find("h1").Text()
+	title = ConvertToString(title, "gbk", "utf-8")
+
+	//获取作者
+	author := doc.Find("p").First().Text()
+	author = ConvertToString(author, "gbk", "utf-8")
+	authors := strings.Split(author, "：")
+	author = authors[1]
+
+	//获取章节列表
+	urlList := []string{}
+	doc.Find("div#list a").Each(func(i int, selection *goquery.Selection) {
+		if i>8 {
+			href,err :=selection.Attr("href")
+			if err!=true {
+				fmt.Println(err)
+			}
+			urlList = append(urlList,href)
+		}
+	})
+
+	return &book{
+		author,
+			title,
+			urlList,
+	}
+}
+
+//获取章节信息
+func chapterInfo(url string) *chapter{
+	doc, err := goquery.NewDocument(url)
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	title := doc.Find("title").Text()
+	title = ConvertToString(title, "gbk", "utf-8")
+	titles := strings.Split(title, "_")
+	title = titles[0]
+
+	content:=	doc.Find("div#content").Text()
+	content = ConvertToString(content, "gbk", "utf-8")
+	content = strings.Replace(content,"聽","　",-1)
+
+	return &chapter{
+		title,
+		content,
+	}
+}
+
+
+func main() {
+	//默认链接
+	baseUrl := "http://www.81xsw.com"
+	//书籍主页
+	bookUrl := baseUrl + "/0_355/"
+	//获取书籍信息
+	bookInfo := bookInfo(bookUrl)
+
+	//fmt.Println(bookInfo.title+"，本书是由:"+bookInfo.author+"所著")
+
+	//循环章节链接，获取内容
+	for _,item := range bookInfo.urlList {
+		chapterUrl:=baseUrl+item
+		go fmt.Println(chapterUrl)
+	}
+
+
+	//chapterUrl := baseUrl+"/0_355/1086799.html"
+	//
+	//chapter := chapterInfo(chapterUrl)
+	//
+	//fmt.Print(chapter)
+
+}
